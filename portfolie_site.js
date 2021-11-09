2482e32dbfa8add4fd2dd7a1e73d4ed667fe9796
JonasRossen <jona7906@stud.kea.dk>
2021-11-09 23:38:46 +0100

    positioned nav

--- portfolie_site.js
+++ portfolie_site.js
@@ -1,4 +1,5 @@
 "use strict";
+
 window.addEventListener("DOMContentLoaded", start);
 
 function start() {
@@ -22,9 +23,97 @@ function loadSVG(svgId) {
       console.log("SVG loaded");
       document.querySelectorAll(`.${svgId}`).forEach((svg) => {
         svg.innerHTML = svgData;
+        changeIds();
       });
     });
 }
+
+function changeIds() {
+  changeIdOnPaths();
+  changeIdOnGs();
+  changeIdOnSvg();
+  animateSvg();
+}
+
+function changeIdOnGs() {
+  let j = 1;
+  console.log("changing ids and g names");
+  document.querySelectorAll("g").forEach((element) => {
+    element.id = "g_" + j;
+    j++;
+    console.log(element);
+  });
+}
+
+function changeIdOnPaths() {
+  let i = 1;
+  console.log("changing ids and path data names");
+  document.querySelectorAll("path").forEach((element) => {
+    element.id = "path_" + i;
+    i++;
+    console.log(element);
+  });
+}
+
+function changeIdOnSvg() {
+  let i = 1;
+  console.log("changing ids and path data names");
+  document.querySelectorAll("svg").forEach((element) => {
+    element.id = "svg_" + i;
+    i++;
+    console.log(element);
+  });
+
+  /* animateSvg("svg_1"); */
+}
+
+function animateSvg(svgId) {
+  console.log(svgId);
+  /*   const properties = {
+    duration: 1000,
+    iterations: "Infinity",
+    direction: "alternate",
+    easing: "ease-in-out",
+  };
+
+  const keyframes = [{ transform: "translate(-60vw,0" }, { transform: "translate(0,0)" }];
+ */
+
+  /*
+  
+  //landing page intro boks
+  const properties = {
+    duration: 1000,
+    iterations: "1",
+    direction: "alternate",
+    easing: "ease-in-out",
+  };
+
+  const keyframes = [{ transform: "translate(0, -5vw)", transform: "scaleY(40%)" }, { transform: "translate(0,0)", transform: "scaleY(100%)" }, { transform: "scaleY(90%)" }, { transform: "scaleY(100%)" }];
+
+  const svg = document.querySelector(`#path_1`); */
+  /*  */
+
+
+  const svg = document.querySelector(`#path_1`);
+  const animation = svg.animate(keyframes, properties);
+  console.log(animation);
+  console.log("WHHHHERE IS THIIIS!!");
+}
+
+/* 
+function landingPageLogoAnimation() {
+  const properties = {
+    duration: 1000,
+    iterations: "none",
+    direction: "alternate",
+    easing: "ease-in-out",
+  };
+
+  const keyframes = [{ transform: "translate(0,41.25vw" }, { transform: "translate(0,0)" }];
+
+  return properties, keyframes;
+} */
 
 /* 
 function hideNavOnScroll() {
